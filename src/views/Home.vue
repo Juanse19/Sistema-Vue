b12fc36d3af396b27b82c89d0f59f9b2feb75f50
sebastian losada <sebastian97-19@hotmail.com>
2020-09-16 17:55:38 +0000

    Sistema basico en Vue

---
+++ src/views/Home.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-container fluid>
+    <v-slide-y-transition mode="out-in">
+      <v-layout column align-center>
+        <img src="https://www.nubetecnologica.com/wp-content/uploads/2017/12/matec-logo.jpg" alt="MATEC" class="mb-5">
+        <blockquote>
+          
+          
+              <div class="home">
+                 <navigation/>
+                  <h1>Bienvenidos</h1>
+                  <p v-if="userLogged">User loggued: {{userLogged}}</p>
+                 </div>
+
+          
+        </blockquote>
+      </v-layout>
+    </v-slide-y-transition>
+  </v-container>
+</template>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+
+<script>
+
+
+
+</script>
